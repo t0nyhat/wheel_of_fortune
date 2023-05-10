470e9ee6723b42c3d91ad9f61d7a96491eadb739
Tony Hat <54199578+t0nyhat@users.noreply.github.com>
2023-05-10 23:44:07 +1000

    first working

---
+++ static/script.js
@@ -0,0 +1,71 @@
+// Immediately invoked function expression
+// to not pollute the global scope
+(function() {
+  const wheel = document.querySelector('.wheel');
+  const game = document.querySelector('.game');
+  const prize = document.querySelector('.prize');
+  const imageprize = document.querySelector('.imageprize');
+  const startButton = document.querySelector('.button');
+  let deg = 0;
+  const degs = [];
+  const prize_images = {
+    0:"sticker.png",
+    18:"unicorn.png",
+    36:"water-game-1.png",
+    54:"tetris.png",
+    72:"fleyta.png",
+    90:"molotok.png",
+    108:"gravura.png",
+    126:"vinni-pooh.png",
+    144:"solt-1.png",
+    162:"pepper.png",
+    180:"fanta.png",
+    198:"planner.png",
+    216:"tr.png",
+    234:"soap-bubble.png",
+    252:"solt-2.png",
+    270:"arm.png",
+    288:"soap-bubble-2.png",
+    306:"swon.png",
+    324:"water-game-2.png",
+    342:"soap-bubble-2.png",
+  }
+  for (let i = 0; i < 360; i+=18) { // выведет 0, затем 1, затем 2
+    degs.push(i);
+  }
+  var selected_deg =0;
+
+  startButton.addEventListener('click', () => {
+    prize.classList.add('d-none');
+    imageprize.src = "";
+    // Disable button during spin
+    startButton.style.pointerEvents = 'none';
+    // Calculate a new rotation between 5000 and 10 000
+    selected_deg = degs[Math.floor(Math.random()*degs.length)];
+    deg = Math.floor(5760 + selected_deg);
+    // Set the transition on the wheel
+    wheel.style.transition = 'all 3s ease-out';
+    // Rotate the wheel
+    wheel.style.transform = `rotate(${deg}deg)`;
+    // Apply the blur
+    wheel.classList.add('blur');
+  });
+
+  wheel.addEventListener('transitionend', () => {
+    // Remove blur
+    wheel.classList.remove('blur');
+    // Enable button when spin is over
+    startButton.style.pointerEvents = 'auto';
+    // Need to set transition to none as we want to rotate instantly
+    wheel.style.transition = 'none';
+    // Calculate degree on a 360 degree basis to get the "natural" real rotation
+    // Important because we want to start the next spin from that one
+    // Use modulus to get the rest value from 360
+    const actualDeg = deg % 360;
+    // Set the real rotation instantly without animation
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+    imageprize.src = "static/prizes/"+prize_images[selected_deg];
+    prize.classList.remove('d-none');
+
+  });
+})();
--- wheel_of_fortune.py
+++ wheel_of_fortune.py
@@ -1,11 +1,21 @@
-from flask import Flask
+"""
+Колесо Фортуны
+"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    """Обычный вид, тестовое колесо"""
+    return render_template('index_test.html')
+
+@app.route('/<int:id>')
+def index_view_game(id):
+    """Призовая игра"""
+    context = {'id': id}
+    return render_template('index.html', **context)
 
 if __name__ == '__main__':
     app.run()
+    
